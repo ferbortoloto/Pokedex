0c9138b1e3228fe2e48f2a782aa47551d2aaab54
ferbortoloto <ferbortoloto@gmail.com>
2024-04-21 21:07:28 -0300

    chore : initial structure

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/layouts/Navbar.vue';
+</script>
+
+<template>
+  <header>
+    <Navbar />
+  </header>
+
+  <RouterView />
+</template>
